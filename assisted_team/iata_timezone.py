de2e4def1b25045c5dd03e519b8c776c982bc780
Oleg Kainov <kainov.oleg@gmail.com>
2019-10-07 19:47:24 +0200

    tz: change DB due to misleading data
    Some airports (even DOH) have no timezone data in openflights dataset,
    so changed to another one which has only TZ info and only one missing
    airport.

--- assisted_team/iata_timezone.py
+++ assisted_team/iata_timezone.py
@@ -1,18 +1,24 @@
 import csv
 
 
-def get_iata_timezone(iata_code: str):
+def get_iata_timezone(iata_code: str) -> str:
     """
-    Funciton used to determine airport's timezone based on the dataset from https://openflights.org/data.html
+    Function used to determine airport's timezone based on the dataset from https://raw.githubusercontent.com/hroptatyr/dateutils/tzmaps/iata.tzmap
+    See https://stackoverflow.com/a/21860150/1657819
     :param iata_code: 3-letter airport IATA code
-    :return:
+    :return: string representation of the timezone in the given airport or empty string if nothing found
     """
-    IATA_INDEX = 4
-    TZ_INDEX = 11
+    IATA_INDEX = 0
+    TZ_INDEX = 1
 
-    with open('airports.dat', 'r', encoding='utf-8') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
+    # TODO: workarounds for the missing codes, needs to be upstreamed
+    if iata_code == 'XNB':
+        return 'Asia/Qatar'
+
+    # TODO: implement some caching to do not re-read the file every time
+    with open('iata.tzmap', 'r', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t')
         for row in reader:
             if row[IATA_INDEX] == iata_code:
                 return row[TZ_INDEX]
-    return None
+    return ''
